79ed8364ca5b8bb1e2160d9a3de1e729b3ad28c0
muham <muhammadmutsolgov2@gmail.com>
2026-08-18 11:23:15 +0300

    pyutohn test

---
+++ testpython.py
@@ -0,0 +1,10 @@
+käyttäjä = input("Anna nimesi")
+print("Hauska tavata, " + käyttäjä + "!")
+
+kanta = int(input("Anna suorakulmion kannan pituus;"))
+korkeus = int(input("Anna suorakulmion korkeus korkeus;"))
+
+ala = kanta*korkeus
+
+#Tulostus
+print(f"Suorakulmion pinta ala on  {ala}")
